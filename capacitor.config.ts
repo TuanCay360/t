e1d8630068bcba6ce5ad7e5b17f8cf4c32d79af1
TuanCay360 <tuandq.22ite@vku.udn.vn>
2025-03-28 14:53:59 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.ktgkaaa',
+  appName: 'ktgkaaa',
+  webDir: 'src'
+};
+
+export default config;
